cc486aca6c2342d3aeff5e62ea74aecfbd3b3f6e
Piyush Kuhad <piyushkuhad@gmail.com>
2021-09-26 19:51:22 +0530

    full commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+
+function HomePage(props) {
+  const featuredEvents = getFeaturedEvents();
+
+  return (
+    <div>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default HomePage;
